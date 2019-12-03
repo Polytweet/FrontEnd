3749a4611765c0820e8e03da3b24769135c84cea
Maxence Bessy <maxence.bessy@gmail.com>
2019-12-03 13:08:59 +0100

    test de merge

--- src/components/CarteCompo.vue
+++ src/components/CarteCompo.vue
@@ -22,32 +22,13 @@
           :colorScale="colorScale"
         >
           <template slot-scope="props" v-if="(communes ? communes.length>0 : false)">
-            <div
-              class="card"
-              v-bind:class="{ 'cardExpand': cardIsExpand }"
-              @mouseup="cardIsExpand=true"
-              @mousedown="cardIsExpand=false"
-            >
-              <div class="card-body">
-                <h5 class="card-title">Département</h5>
-                <h6 class="card-subtitle mb-2 text-muted">{{props.currentItem.name}}</h6>
-                <div
-                  v-if="props.currentItem.value!=undefined && props.currentItem.extraValues!=undefined && props.currentItem.extraValues[0]!=undefined"
-                >
-                  <span>{{props.currentItem.value}}hab</span>
-                  <br />
-                  <span>{{props.currentItem.extraValues[0].value}}{{props.currentItem.extraValues[0].metric}}</span>
-                </div>
-              </div>
-            </div>
-
-            <!--   <l-info-control
+            <l-info-control
               :item="props.currentItem"
               :unit="props.unit"
               title="Département"
               placeholder="Survolez un département"
               position="topleft"
-            ></l-info-control>-->
+            ></l-info-control>
           </template>
         </l-choropleth-layer>
       </div>
@@ -57,7 +38,7 @@
 
  
 <script>
-import { ChoroplethLayer } from "vue-choropleth";
+import { InfoControl, ChoroplethLayer } from "vue-choropleth";
 
 import { LMap, LTileLayer } from "vue2-leaflet";
 import axios from "axios";
@@ -68,6 +49,7 @@ export default {
   components: {
     LMap,
     LTileLayer,
+    "l-info-control": InfoControl,
     "l-choropleth-layer": ChoroplethLayer
   },
   props: {
@@ -80,7 +62,6 @@ export default {
   },
   data() {
     return {
-      cardIsExpand: false,
       url:
         "https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png",
       zoom: 6,
@@ -131,45 +112,41 @@ export default {
     // this.getDataFromGeoJson(69)
   },
   created() {
-    window.addEventListener("click", () => {
+    window.addEventListener('click', () => {
       switch (this.$store.state.currentFilter) {
         case "franceN":
           this.zoomUpdated(6);
           this.getDeptDataFromGeoJson();
-          this.value.key = "rien";
-          this.value.metric = "";
+          this.value.key = 'rien';
+          this.value.metric = '';
           this.extraValues = [];
-          this.$store.state.currentFilter = "france";
+          this.$store.state.currentFilter = 'france'
           break;
         case "departementN":
           this.zoomUpdated(7);
           this.getDeptDataFromGeoJson();
-          this.value.key = "rien";
-          this.value.metric = "";
+          this.value.key = 'rien';
+          this.value.metric = '';
           this.extraValues = [];
-          this.$store.state.currentFilter = "departement";
+          this.$store.state.currentFilter = 'departement'
           break;
         case "communeN":
           this.zoomUpdated(10);
-          this.value.key = "population";
-          this.value.metric = "hab";
-          this.extraValues = [
-            {
-              key: "superficie",
-              metric: " ha"
-            }
-          ];
-          this.$store.state.currentFilter = "commune";
+          this.value.key = 'population';
+          this.value.metric = 'hab';
+          this.extraValues = [{
+            key: "superficie",
+            metric: " ha"
+          }];
+          this.$store.state.currentFilter = 'commune'
           break;
       }
-    });
-    window.addEventListener("load", () => {
-      document
-        .getElementsByClassName("getPosition")[0]
-        .addEventListener("click", () => {
-          this.getCurrentLocation();
-        });
-    });
+    })
+    window.addEventListener('load', () => {
+      document.getElementsByClassName('getPosition')[0].addEventListener('click', () => {
+        this.getCurrentLocation()
+      })
+    })
   },
   methods: {
     zoomUpdated(zoom) {
@@ -182,30 +159,18 @@ export default {
       this.bounds = bounds;
     },
     getCoord(event) {
-      if (this.$store.state.currentFilter === "departement") {
-        axios
-          .get(
-            "https://api.opencagedata.com/geocode/v1/json?q=" +
-              event.latlng.lat +
-              "+" +
-              event.latlng.lng +
-              "&key=6cb782be82c646cfb05d9471b7ca2961"
-          )
-          .then(res => {
-            console.log(
-              res.data.results[0].components.postcode.substring(0, 2)
-            );
-            (this.center = [event.latlng.lat, event.latlng.lng]),
-              (this.$store.state.currentFilter = "communeN");
+      if (this.$store.state.currentFilter === 'departement') {
+        axios.get('https://api.opencagedata.com/geocode/v1/json?q=' + event.latlng.lat + '+' + event.latlng.lng + '&key=6cb782be82c646cfb05d9471b7ca2961').then((res) => {
+          console.log(res.data.results[0].components.postcode.substring(0,2))
+          this.center = [event.latlng.lat, event.latlng.lng],
+          this.$store.state.currentFilter = 'communeN'
 
-            this.getDataFromGeoJson(
-              res.data.results[0].components.postcode.substring(0, 2)
-            );
-          });
+          this.getDataFromGeoJson(res.data.results[0].components.postcode.substring(0,2))
+        })
       }
-      if (this.$store.state.currentFilter === "france") {
-        (this.center = [event.latlng.lat, event.latlng.lng]),
-          (this.$store.state.currentFilter = "departementN");
+      if (this.$store.state.currentFilter === 'france') {
+        this.center = [event.latlng.lat, event.latlng.lng],
+          this.$store.state.currentFilter = 'departementN'
       }
     },
 
@@ -217,24 +182,12 @@ export default {
             console.log(position);
 
             this.center = [position.coords.latitude, position.coords.longitude];
-            axios
-              .get(
-                "https://api.opencagedata.com/geocode/v1/json?q=" +
-                  position.coords.latitude +
-                  "+" +
-                  position.coords.longitude +
-                  "&key=6cb782be82c646cfb05d9471b7ca2961"
-              )
-              .then(res => {
-                console.log(
-                  res.data.results[0].components.postcode.substring(0, 2)
-                );
-                this.$store.state.currentFilter = "communeN";
+            axios.get('https://api.opencagedata.com/geocode/v1/json?q=' + position.coords.latitude + '+' + position.coords.longitude + '&key=6cb782be82c646cfb05d9471b7ca2961').then((res) => {
+              console.log(res.data.results[0].components.postcode.substring(0,2))
+              this.$store.state.currentFilter = 'communeN'
 
-                this.getDataFromGeoJson(
-                  res.data.results[0].components.postcode.substring(0, 2)
-                );
-              });
+              this.getDataFromGeoJson(res.data.results[0].components.postcode.substring(0,2))
+            })
           },
           function(error) {
             alert(error.message);
@@ -272,43 +225,41 @@ export default {
         `
       });
       this.communes = resApollo.data.communes;
-      this.communesInfo = [];
+      this.communesInfo = []
       resApollo.data.communes.forEach(element => {
-        element.properties.rien = "";
+        element.properties.rien = ''
         this.communesInfo.push(element.properties);
       });
-      console.log(this.communes);
-      this.updateZoom();
+      console.log(this.communes)
+      this.updateZoom()
     },
-    updateZoom() {
+    updateZoom () {
       switch (this.$store.state.currentFilter) {
         case "franceN":
           this.zoomUpdated(6);
           this.getDeptDataFromGeoJson();
-          this.value.key = "rien";
-          this.value.metric = "";
+          this.value.key = 'rien';
+          this.value.metric = '';
           this.extraValues = [];
-          this.$store.state.currentFilter = "france";
+          this.$store.state.currentFilter = 'france'
           break;
         case "departementN":
           this.zoomUpdated(7);
           this.getDeptDataFromGeoJson();
-          this.value.key = "rien";
-          this.value.metric = "";
+          this.value.key = 'rien';
+          this.value.metric = '';
           this.extraValues = [];
-          this.$store.state.currentFilter = "departement";
+          this.$store.state.currentFilter = 'departement'
           break;
         case "communeN":
           this.zoomUpdated(10);
-          this.value.key = "population";
-          this.value.metric = "hab";
-          this.extraValues = [
-            {
-              key: "superficie",
-              metric: " ha"
-            }
-          ];
-          this.$store.state.currentFilter = "commune";
+          this.value.key = 'population';
+          this.value.metric = 'hab';
+          this.extraValues = [{
+            key: "superficie",
+            metric: " ha"
+          }];
+          this.$store.state.currentFilter = 'commune'
           break;
       }
     },
@@ -332,19 +283,17 @@ export default {
         `
       });
       this.communes = resApollo.data.departements;
-      this.communes.forEach(c => {
-        if (
-          c.geometry.coordinates ? c.geometry.coordinates.length === 0 : false
-        ) {
-          c.geometry.coordinates = c.geometry.coordinatesMulti;
+      this.communes.forEach((c) => {
+        if (c.geometry.coordinates ? c.geometry.coordinates.length === 0 : false) {
+          c.geometry.coordinates = c.geometry.coordinatesMulti
         }
-      });
-      this.communesInfo = [];
+      })
+      this.communesInfo = []
       resApollo.data.departements.forEach(element => {
-        element.properties.rien = "";
+        element.properties.rien = ''
         this.communesInfo.push(element.properties);
       });
-      this.updateZoom();
+      this.updateZoom()
     }
   }
 };
@@ -356,37 +305,5 @@ export default {
 .vue2leaflet-map {
   height: 100vh;
   width: 100vw;
-}
-
-.card {
-  z-index: 1000;
-  width: 20%;
-  position: absolute;
-  left: 3%;
-  top: 3%;
-  min-height: 66px;
-  box-shadow: 0 1.25rem 1.562rem -1.25rem rgba(0, 0, 0, 0.2);
-  border: 1px solid #eaf1f3;
-  background-color: #fff !important;
-}
-.card-body {
-  padding: 1rem;
-}
-
-@media screen and (max-width: 540px) {
-  .card {
-    position: absolute;
-    top: auto;
-    left: 0;
-    bottom: 0;
-    width: 100vw;
-    min-height: 75px;
-    max-height: 75px;
-  }
-  .cardExpand {
-    height: 50vh;
-    min-height: 50vh;
-    max-height: 50vh;
-  }
 }
 </style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,81 +1,64 @@
 <template>
   <div>
+    <!-- Sidebar -->
+    <aside :class="{ clodedSideNav: openSideNav}">
+      <ul class="list-unstyled components">
+        <li class="asideLink d-flex align-items-center border-bottom title-link">
+          <a href="/">Polytweet</a>
+        </li>
+        <li class="asideLink d-flex align-items-center">
+          <i class="far fa-list-alt"></i>
+          <span class="ml-4">Fuck</span>
+        </li>
+        <li class="asideLink d-flex align-items-center">
+          <i class="far fa-compass"></i>
+          <span class="ml-4">Le</span>
+        </li>
+        <li class="asideLink d-flex align-items-center">
+          <i class="far fa-map"></i>
+          <span class="ml-4">C</span>
+        </li>
+        <li class="asideLink d-flex align-items-center">
+          <i class="fas fa-chart-line"></i>
+          <span class="ml-4">S</span>
+        </li>
+        <li class="asideLink d-flex align-items-center">
+          <i class="far fa-heart"></i>
+          <span class="ml-4">S</span>
+        </li>
+      </ul>
+    </aside>
+
+    <!-- Overlay -->
+    <div class="overlay" :class="{ active: !openSideNav}" @click="openSideNav = !openSideNav"></div>
+
     <!-- Search Nav -->
-    <div class="searchNavContainer mx-auto" @mouseleave="inputSearchFocus = false">
+    <div class="searchNavContainer mx-auto">
       <b-navbar
         toggleable="sm"
         type="light"
         variant="light"
-        class="col-lg-6 col-md-9 col-11 searchNav"
+        class="col-lg-6 col-md-9 col-10 searchNav"
       >
-        <div class="row align-items-center w-100 col-12">
-          <a
-            href="/"
-            class="sidebarButton col-2 col-md-2 col-lg-1 d-flex align-items-center justify-content-center"
+        <div class="row align-items-center w-100">
+          <div
+            class="sidebarButton col-3 col-md-2 justify-content-center d-flex align-items-center"
+            @click="toggleNav()"
           >
-            <img alt="Vue logo" src="../assets/logo.png" width="40" />
-          </a>
-          <div class="col-lg-11 col-10 d-flex align-items-center inputContainer">
-            <!-- <input
-              id="searchBar"
-              class="inputSearchbar w-100 effect-8"
-              type="text"
-              placeholder="Choisissez une actualité"
-              @focus="inputSearchFocus = true"
-              v-model="filter"
-            />-->
+            <i class="fas fa-bars"></i>
+          </div>
+          <div class="col-9 col-md-10 d-flex align-items-center inputContainer">
             <input
               id="searchBar"
-              class="inputSearchbar effect-focus-input w-100"
+              class="inputSearchbar w-100"
               type="text"
-              placeholder="Choisissez une actualité"
-              @focus="inputSearchFocus = true"
-              v-model="filter"
+              placeholder="Entrez votre recherche"
             />
-            <span class="focus-bg"></span>
-          </div>
-        </div>
-        <!-- News Part -->
-        <div class="col-12 info-part" v-bind:class="{ searchExpand: inputSearchFocus }">
-          <br />
-          <!-- NewsSelected -->
-          <div class="newsFilter d-flex align-items-start">
-            <div class="d-flex flex-wrap w-100">
-              <!-- Chips -->
-              <div class="chip mt-1" v-for="chips in chipsList" v-bind:key="chips.title">
-                <span class="mx-auto">{{chips.title.split(' ').slice(0,2).join(' ')}}</span>
-                <i
-                  class="fas fa-times-circle ml-1 float-right text-right"
-                  v-on:click="removeToChipsList(chips)"
-                ></i>
+            <div class="col-2 col-md-1">
+              <div class="circle d-flex align-items-center justify-content-center">
+                <i class="fas fa-search btn-search mx-2"></i>
               </div>
-              <!-- /Chips -->
             </div>
-          </div>
-
-          <!-- List Header -->
-          <div class="newsList mt-4">
-            <div class="d-flex justify-content-between">
-              <h6 class="mr-3 text-left">News</h6>
-              <h6 v-on:click="exportData(getNews,filter)">Export</h6>
-              <h6 class="mr-3 text-right">{{getNews.length}} résultats</h6>
-            </div>
-            <!-- List -->
-
-            <table class="d-flex listWrap table table-striped">
-              <tbody>
-                <tr v-for="data in getNews" v-bind:key="data._id">
-                  <td
-                    :disabled="data.chipsSelected==true"
-                    v-bind:class="{ chipSelected: data.chipsSelected==true }"
-                    v-on:click="addToChipsList(data)"
-                    class="newsContent d-flex justify-content-center align-items-center"
-                  >{{data.title}}</td>
-                </tr>
-              </tbody>
-            </table>
-
-            <!-- /List -->
           </div>
         </div>
       </b-navbar>
@@ -102,7 +85,7 @@
         <div
           class="row"
           :class="{ filterActive: currentFilter=='departement'}"
-          @click="currentFilter = 'departement'"
+          @click="currentFilter = 'departementN'"
         >
           <i class="far fa-circle" v-if="currentFilter!='departement'"></i>
           <i class="far fa-check-circle" v-if="currentFilter=='departement'"></i>
@@ -114,7 +97,7 @@
         <div
           class="row"
           :class="{ filterActive: currentFilter=='commune'}"
-          @click="currentFilter = 'commune'"
+          @click="currentFilter = 'communeN'"
         >
           <i class="far fa-circle" v-if="currentFilter!='commune'"></i>
           <i class="far fa-check-circle" v-if="currentFilter=='commune'"></i>
@@ -122,8 +105,10 @@
         </div>
       </div>
     </div>
-    <div class="getPosition d-flex justify-content-center align-items-center">
-      <i class="fas fa-map"></i>
+    <div
+      class="getPosition d-flex justify-content-center align-items-center"
+    >
+    <i class="fas fa-map-marker-alt"></i>
     </div>
   </div>
 </template>
@@ -132,112 +117,35 @@
  
 
 <script>
-import json from "../assets/json/news.json";
-import gql from "graphql-tag";
-
 export default {
   name: "SearchBar",
 
   data() {
     return {
       wrapperExp: false,
-      currentFilter: "commune",
-      inputSearchFocus: false,
-      chipsList: [],
-      filter: "",
-      news: json
+      openSideNav: true,
     };
   },
-  watch: {
-    currentFilter: function() {
-      this.$emit("filterData", this.currentFilter);
-    }
-  },
-
-  created() {
-    this.getDataFromNews();
-  },
-  methods: {
-    //Ajoute une news au filtres (chips)
-    addToChipsList(data) {
-      if (data.chipsSelected != true) {
-        this.chipsList.push(data);
-        this.news[this.news.findIndex(x => x._id === data._id)][
-          "chipsSelected"
-        ] = true;
-      }
-    },
-    //On click icone croix -> chercher l'object et le retire de la list des chips
-    removeToChipsList(data) {
-      this.news[this.news.findIndex(x => x._id === data._id)][
-        "chipsSelected"
-      ] = false;
-      this.chipsList.splice(
-        this.chipsList
-          .map(function(e) {
-            return e.title;
-          })
-          .indexOf(data.title),
-        1
-      );
-    },
-    async getDataFromNews() {
-      let resApollo = await this.$apollo.query({
-        query: gql`
-          query {
-            news {
-              _id
-              title
-              url
-            }
-          }
-        `
-      });
-      console.log(resApollo);
-      this.news = resApollo.data.news;
-    },
-
-    /*Pour TensorFlow JS*/
-    exportData(data, filter) {
-      let result = [];
-      data.forEach(e => {
-        let indent = e.title.toLowerCase().includes(filter) ? filter : "none";
-        result.push({ text: e.title, indent: indent });
-      });
-
-      const dataJson = JSON.stringify(result);
-      const blob = new Blob([dataJson], { type: "text/plain" });
-      const e = document.createEvent("MouseEvents"),
-        a = document.createElement("a");
-      a.download = "comment-giletJaune-training.json";
-      a.href = window.URL.createObjectURL(blob);
-      a.dataset.downloadurl = ["text/json", a.download, a.href].join(":");
-      e.initEvent(
-        "click",
-        true,
-        false,
-        window,
-        0,
-        0,
-        0,
-        0,
-        0,
-        false,
-        false,
-        false,
-        false,
-        0,
-        null
-      );
-      a.dispatchEvent(e);
-    }
-  },
   computed: {
-    getNews() {
-      var news = this.news.filter(player => {
-        return player.title.toLowerCase().includes(this.filter.toLowerCase());
-      });
-      return news;
+    currentFilter: {
+      get () {
+        return this.$store.state.currentFilter
+      },
+      set (newV) {
+        this.$store.state.currentFilter = newV
+      }
+    }
+  },
+  watch: {
+    openSideNav: function() {
+      this.$emit("sideNavState", this.openSideNav);
+    }
+  },
+  mounted() {},
+  created() {},
+  methods: {
+    toggleNav() {
+      this.openSideNav = !this.openSideNav;
     }
   }
 };
@@ -245,30 +153,72 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.effect-focus-input ~ .focus-bg {
+/* =============================
+            SIDEBAR
+ ============================= */
+aside {
+  display: block;
+  min-width: 250px;
+  max-width: 250px;
+  background: #fff;
+  color: #798186c2;
+  transition: all 0.3s;
+  height: 100vh;
   position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background-color: #ededed;
+  z-index: 999;
+}
+aside.clodedSideNav {
+  margin-left: -260px;
+}
+
+/*Title ( Polytweet)*/
+aside li.title-link {
+  height: 95px !important;
+  /*  margin-left: 15px;
+  margin-right: 10px;
+  margin-bottom: 10px; */
+  justify-content: center;
+  padding-left: 0px !important;
+}
+aside li.title-link a {
+  text-decoration: none;
+  color: #6892fc;
+  font-family: "Airbnb Cereal App Black";
+  font-size: 24px;
+  /*  margin-left: -15px; */
+}
+
+/*Other Links*/
+.asideLink {
+  height: 50px;
+  padding-left: 15%;
+  cursor: pointer;
+}
+.asideLink i {
+  font-size: 20px;
+  color: #798186c2;
+}
+.asideLink.border-bottom {
+  border-bottom: 1px solid #e8eaed;
+}
+.asideLink:not(.title-link):hover {
+  background-color: #e8eaed;
+}
+
+/*Overlay*/
+.overlay {
+  display: none;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.7);
+  z-index: 997;
   opacity: 0;
-  transition: 0.5s;
-  z-index: -1;
+  transition: all 0.5s ease-in-out;
 }
-.effect-focus-input:focus ~ .focus-bg {
-  transition: 0.5s;
+.overlay.active {
+  display: block;
   opacity: 1;
-}
-.effect-focus-input {
-  border: 0;
-  padding: 7px 15px;
-  border: 1px solid #ccc;
-  position: relative;
-  background: transparent;
-}
-:focus {
-  outline: none;
 }
 
 /* =============================
@@ -284,14 +234,12 @@ export default {
   position: absolute;
   top: 3%;
   z-index: 510;
-  border-radius: 10px;
+  border-radius: 25em;
   cursor: pointer;
-  box-shadow: 0 1.25rem 1.562rem -1.25rem rgba(0, 0, 0, 0.2);
+  box-shadow: 0 1.25rem 1.562rem -1.25rem rgba(0, 0, 0, 0.4);
   border: 1px solid #eaf1f3;
   background-color: #fff !important;
   padding: 0.75rem 1rem;
-  /*New*/
-  flex-wrap: wrap;
 }
 
 /*Button who open the sidenav*/
@@ -312,11 +260,11 @@ export default {
   margin-right: 8px;
   font-size: 1em;
   font-weight: bold;
-  border-bottom: none;
+  border-bottom: #798186c2 solid 2px;
   transition: 0.3s;
 }
 .inputSearchbar::placeholder {
-  color: #2c3e50;
+  color: #798186c2;
   font-size: 1em;
   font-weight: bold;
 }
@@ -333,84 +281,9 @@ export default {
   font-size: 1.25em;
 }
 
-/* News part */
-.info-part {
-  height: 0px;
-  overflow: hidden;
-  -moz-transition: height 0.5s;
-  -ms-transition: height 0.5s;
-  -o-transition: height 0.5s;
-  -webkit-transition: height 0.5s;
-  transition: height 0.5s;
-}
-.searchExpand {
-  height: 65vh;
-  -moz-transition: height 0.5s;
-  -ms-transition: height 0.5s;
-  -o-transition: height 0.5s;
-  -webkit-transition: height 0.5s;
-  transition: height 0.5s;
-}
-
-/*Chips*/
-.chip {
-  border-radius: 16px;
-  font-size: 14px;
-  font-weight: 600;
-  height: auto;
-  width: 175px;
-  text-align: center;
-  border: 2px solid rgba(51, 51, 51, 0.46);
-  color: rgba(51, 51, 51, 0.46);
-  -ms-flex-align: center;
-  align-items: center;
-  justify-content: space-between;
-  display: inline-flex;
-  line-height: 20px;
-  padding: 0 12px;
-}
-.chip .text-left {
-  height: 20px;
-  overflow: hidden;
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-}
-
-/* News List */
-.listWrap {
-  max-height: 55vh;
-  overflow: scroll;
-  overflow-x: unset;
-}
-.newsContent {
-  color: rgba(51, 51, 51);
-  border: none;
-  font-weight: 500;
-  height: 90px;
-}
-.chipSelected {
-  background-color: #6892fc;
-  color: white;
-  cursor: not-allowed;
-}
-
 /* =============================
            FILTRES
  ============================= */
-.getPosition {
-  background: #fff;
-  width: 66px;
-  height: 66px;
-  border-radius: 25em !important;
-  position: absolute;
-  right: 6%;
-  top: 3%;
-  z-index: 509;
-  box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2),
-    0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
-  cursor: pointer;
-}
 .filterSideNav {
   background: #fff;
   width: 66px;
@@ -420,7 +293,7 @@ export default {
   position: absolute;
   right: 1%;
   top: 3%;
-  z-index: 509;
+  z-index: 510;
   box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2),
     0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
   max-height: 72px;
@@ -448,8 +321,7 @@ export default {
   word-break: break-word;
   font-size: 0.75rem;
 }
-.filterSideNav i,
-.getPosition i {
+.filterSideNav i {
   font-size: 22px;
   color: #798186c2;
 }
@@ -478,24 +350,15 @@ export default {
   }
   .filterSideNav {
     width: 50px;
-    top: 24%;
+    top: 15%;
   }
-  .filterItemContainer,
-  .getPosition {
+  .filterItemContainer {
     width: 50px;
     height: 50px;
-  }
-  .getPosition {
-    top: 15%;
-    right: 1%;
   }
   .filterSideNav h6 {
     word-break: break-word;
     font-size: 0.6rem;
-  }
-  /* News List*/
-  .newsContent {
-    height: 125px;
   }
 }
 </style>
