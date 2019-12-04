2c1b0f271d7724facc8e56bf7dde292fc265a895
Arnaud <arnaudvolp@gmail.com>
2019-12-04 17:17:21 +0100

    merge carte info

--- src/components/HomeCompo.vue
+++ src/components/HomeCompo.vue
@@ -1,29 +1,33 @@
 <template>
-  <div class="home">
-    <div class="svgHome">
-      <img
-        class="svgBackground"
-        src="../assets/svg/homePage.svg"
-        width="100%"
-        alt="Background home"
-      />
-    </div>
-    <div class="row">
-      <div class="textWrapper col-md-6 col-lg-4">
-        <h3>
-          Polytweet, la solution pour connaître
-          <br />tous les hastags à suivre
-        </h3>
-        <p
-          class="mt-3"
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus officiis enim sit impedit necessitatibus, fuga, obcaecati excepturi eos dolore reiciendis quasi nemo! Commodi asperiores a sapiente. Sunt nam necessitatibus minima.</p>
-        <router-link to="/carte">
-          <button class="btn btn-primary mt-4">Découvrir l'application</button>
-        </router-link>
+  <div>
+    <div class="home">
+      <div class="svgHome">
+        <img
+          class="svgBackground"
+          src="../assets/svg/homePage.svg"
+          width="100%"
+          alt="Background home"
+        />
+      </div>
+      <div class="row">
+        <div class="textWrapper col-md-6 col-lg-4">
+          <h3>
+            Polytweet, la solution pour connaître
+            <br />tous les hastags à suivre
+          </h3>
+          <p
+            class="mt-3"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus officiis enim sit impedit necessitatibus, fuga, obcaecati excepturi eos dolore reiciendis quasi nemo! Commodi asperiores a sapiente. Sunt nam necessitatibus minima.</p>
+          <router-link to="/carte">
+            <button class="btn btn-primary mt-4">Découvrir l'application</button>
+          </router-link>
+        </div>
       </div>
     </div>
+    <div class="compteur">
+      <h1>{{dataCompteur}}</h1>
+    </div>
   </div>
-
   <!-- 
   explication projet
   mention legales
@@ -33,21 +37,40 @@
 <script>
 export default {
   name: "HomeCompo",
-
+  data() {
+    return {
+      dataCompteur: 175302
+    };
+  },
   props: {},
-  methods: {}
+  methods: {
+    regenerateDataCompteur() {
+      console.log("regen");
+    }
+  },
+  watch: {
+    dataCompteur: () => {
+      var digits = ("" + this.dataCompteur).split("");
+      console.log(digits);
+    }
+  },
+  mounted() {
+    this.regenerateDataCompteur();
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.home {
+/* Home */
+.home,
+.compteur {
   height: 100vh;
   width: 100vw;
+}
+.home {
   display: flex;
   align-items: center;
-  position: absolute;
-  top: 0;
   max-width: 100%;
   overflow: hidden;
   z-index: -1;
@@ -78,8 +101,17 @@ export default {
     margin-left: 15px;
   }
 }
+
+/* Compteur */
+.compteur {
+  background-color: #dedede;
+  display: flex;
+  align-items: center;
+}
+
 /*Mobile*/
 @media screen and (max-width: 719px) {
+  /* Home */
   .textWrapper {
     position: absolute;
     bottom: 10%;
